f96e5f83be34f1e3778004f3153f8cc7417df2af
ha-kun <haakunn@gmail.com>
2019-05-23 19:01:18 +0900

    スコア入力画面を作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,114 @@
+/** @jsx jsx */
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { css, jsx } from '@emotion/core';
+import { Button, Card, Divider, Grid, Input, Segment } from 'semantic-ui-react';
 
 const App: React.FC = () => {
+  const wrapperCard = css`
+    background-color: #282c34 !important;
+    color: white !important;
+  `;
+  const wrapperOr = css`
+    > .or:before {
+      background-color: #282c34 !important;
+      color: white !important;
+    }
+  `;
+  const wrapperInput = css`
+    > input {
+      text-align: center !important;
+      color: white !important;
+    }
+  `;
+
+  const wrapperWinButton = css`
+    background-color: green !important;
+    color: white !important;
+  `;
+
+  const wrapperScore = css`
+    > p {
+      height: 60vh;
+      font-size: 50vh;
+      line-height: 60vh;
+    }
+  `;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Segment inverted color="black">
+      <Grid columns={2} relaxed="very">
+        <Grid.Column>
+          <Card centered fluid css={wrapperCard}>
+            <Card.Content textAlign="center">
+              <Input
+                css={wrapperInput}
+                transparent
+                placeholder="Name"
+                size="massive"
+              />
+            </Card.Content>
+            <Card.Content textAlign="center" css={wrapperScore}>
+              <p>0</p>
+            </Card.Content>
+            <Card.Content textAlign="center">
+              <Grid columns="equal">
+                <Grid.Row columns="equal">
+                  <Grid.Column>
+                    <Button.Group css={wrapperOr}>
+                      <Button positive> + </Button>
+                      <Button.Or />
+                      <Button positive> - </Button>
+                    </Button.Group>
+                  </Grid.Column>
+                  <Grid.Column>
+                    <Button inverted color="green" css={wrapperWinButton}>
+                      Win
+                    </Button>
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+        <Grid.Column>
+          <Card centered fluid css={wrapperCard}>
+            <Card.Content textAlign="center">
+              <Input
+                css={wrapperInput}
+                transparent
+                placeholder="Name"
+                size="massive"
+              />
+            </Card.Content>
+            <Card.Content textAlign="center" css={wrapperScore}>
+              <p>0</p>
+            </Card.Content>
+            <Card.Content textAlign="center">
+              <Grid columns="equal">
+                <Grid.Row columns="equal">
+                  <Grid.Column>
+                    <Button.Group css={wrapperOr}>
+                      <Button positive> + </Button>
+                      <Button.Or />
+                      <Button positive> - </Button>
+                    </Button.Group>
+                  </Grid.Column>
+                  <Grid.Column>
+                    <Button inverted color="green" css={wrapperWinButton}>
+                      Win
+                    </Button>
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+      </Grid>
+
+      <Divider vertical inverted>
+        VS
+      </Divider>
+    </Segment>
   );
 };
 
